c6187fa8d0f7431beb4669ad0729d8a70a8a0e92
heshuai <276575758@qq.com>
2017-05-18 18:04:49 +0800

    debug switch project

--- miraPipeline/maya/switch_project/switch_project.py
+++ miraPipeline/maya/switch_project/switch_project.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
 from PySide import QtGui, QtCore
 import maya.OpenMaya as OpenMaya
 from miraLibs.mayaLibs.MayaToolBar import MayaToolBar
 from miraLibs.pipeLibs import pipeMira, pipeHistory
 from miraLibs.mayaLibs import get_maya_globals
 from miraLibs.pipeLibs.pipeMaya import get_current_project
+import miraCore
 
 
 def get_current_project_from_outside():
@@ -28,24 +30,39 @@ class ProjectButton(QtGui.QPushButton):
         self.projects = pipeMira.get_projects()
         self.project = get_current_project_from_outside()
         self.setFixedHeight(22)
+        self.set_icon()
         self.set_style_sheet()
         self.create_menu()
         self.set_project(self.project)
         OpenMaya.MEventMessage.addEventCallback("SceneOpened", self.set_project_when_scene_opened)
 
     def set_style_sheet(self):
-        self.setStyleSheet("background:qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,"
-                           "stop: 0 purple, stop: 0.5 #FF00FF,"
-                           "stop: 1.0 purple);"
-                           "color:#FFFFFF;"
-                           "text-align:left;"
-                           "font-size:10pt;"
-                           "font-family:Microsoft YaHei;"
-                           "border-radius: 5px;"
-                           "padding: 1px;"
-                           "border-style: outset;"
-                           "border-width: 1px;"
-                           "border-color: #AA00AA;")
+        # self.setStyleSheet("background:qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,"
+        #                    "stop: 0 purple, stop: 0.5 #FF00FF,"
+        #                    "stop: 1.0 purple);"
+        #                    "color:#FFFFFF;"
+        #                    "text-align:left;"
+        #                    "font-size:10pt;"
+        #                    "font-family:Microsoft YaHei;"
+        #                    "border-radius: 0px;"
+        #                    "padding: 1px;"
+        #                    "border-style: outset;"
+        #                    "border-width: 1px;"
+        #                    "border-color: #AA00AA;")
+        self.setStyleSheet("QPushButton{background: transparent;color: #ff8c00; "
+                           "font-size:10pt;font-family:Microsoft YaHei;border-width:0px;"
+                           "border-color: #444444; text-align: left;border-radius: 0px;padding-right: 0px;}"
+                           "QPushButton:hover{background: #222222;}")
+
+    def set_icon(self):
+        icon_dir = miraCore.get_icons_dir()
+        icon_path = os.path.join(icon_dir, "project_icon", "%s.png" % self.project)
+        if not os.path.isfile(icon_path):
+            icon_path = os.path.join(icon_dir, "project_icon", "company.png")
+        icon_path = icon_path.replace("\\", "/")
+        icon = QtGui.QIcon(icon_path)
+        self.setIcon(icon)
+        self.setIconSize(QtCore.QSize(50, self.height()))
 
     def create_menu(self):
         self.menu = QtGui.QMenu()
@@ -65,6 +82,7 @@ class ProjectButton(QtGui.QPushButton):
             text = u" 当前项目: %s" % self.project
             self.setText(text)
             self.record_history()
+            self.set_icon()
         else:
             QtGui.QMessageBox.warning(None, "Warming Tip", "%s is not a unknown project." % project)
 
