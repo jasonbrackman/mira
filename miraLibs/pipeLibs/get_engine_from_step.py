01c22dbde0b0fa1640db54ca90fec64af5b581bc
heshuai <276575758@qq.com>
2017-07-19 21:05:03 +0800

    debug task start,check up step published file first.

--- miraLibs/pipeLibs/get_engine_from_step.py
+++ miraLibs/pipeLibs/get_engine_from_step.py
@@ -7,6 +7,6 @@ def get_engine_from_step(step):
         return "photoshop"
     if step in ["Comp"]:
         engine = "nuke"
-    elif step in ["LowMdl", "MidMdl", "HighMdl", "Shd", "MidRig", "Rig", "Hair", "Group"]:
+    elif step in ["LowMdl", "MidMdl", "HighMdl", "Shd", "MidRig", "Rig", "Hair", "Group", "HighRig"]:
         engine = "maya"
     return engine
