aef2e8f257b384287b982f467ea2f77f395bd480
heshuai <276575758@qq.com>
2017-07-24 10:30:38 +0800

    debug  shd multi uv set.

---
+++ miraPipeline/pipeline/preflight/check_options/maya/check_shd_uvset.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import maya.cmds as mc
+from BaseCheck import BaseCheck
+
+
+class check_shd_uvset(BaseCheck):
+
+    def run(self):
+        self.error_list = self.get_error_list()
+        if self.error_list:
+            self.fail_check(u"有多个uv set存在")
+        else:
+            self.pass_check(u"没有多个uv set存在")
+
+    @staticmethod
+    def get_error_list():
+        uv_sets = mc.polyUVSet(q=1, allUVSets=1)
+        if len(uv_sets) > 1:
+            return uv_sets
