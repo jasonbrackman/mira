5ae13eb136c34dea0678e5df90e4b9da1bc4899f
heshuai <276575758@qq.com>
2017-08-11 19:41:56 +0800

    debug qc.

--- miraPipeline/maya/QC/qc.py
+++ miraPipeline/maya/QC/qc.py
@@ -29,7 +29,7 @@ def qcpublish(step):
     mod.main()
 
 
-def post_qcpublish(context):
+def post_qcpublish(context, version_file):
     from miraLibs.dbLibs import db_api
     from miraLibs.pipeLibs.pipeDb import task_from_db_path
     db = db_api.DbApi(context.project).db_obj
@@ -41,11 +41,7 @@ def post_qcpublish(context):
     db.update_task_status(task, "Supervisor Review")
     db.upload_thumbnail(task, context.work_image_path)
     # upload version
-    not_playblast_step = pipeMira.get_studio_value(context.project, "not_playblast_step")
-    if context.step in not_playblast_step:
-        db.upload_version(task, file_path=context.work_path)
-    else:
-        db.upload_version(task, media_path=context.work_video_path, file_path=context.work_path)
+    db.upload_version(task, media_path=version_file, file_path=context.work_path)
     logger.info("Upload version done.")
     # update task
     db.update_file_path(task, work_file_path=context.work_path)
@@ -59,6 +55,10 @@ def submit_other(other_dir, files):
         base_name = os.path.basename(f)
         to_other_path = join_path.join_path2(other_dir, base_name)
         copy.copy(f, to_other_path)
+
+
+def submit_version():
+    pass
 
 
 class TextLabel(QLabel):
@@ -119,6 +119,7 @@ class CellQCWidget(QWidget):
 
     def fail(self):
         self.set_status("fail")
+        raise RuntimeError("Something wrong with this step")
 
     def success(self):
         self.set_status("success")
@@ -131,7 +132,21 @@ class QC(QDialog):
     def __init__(self, parent=None):
         super(QC, self).__init__(parent)
         self.setWindowTitle("QC Publish")
-        self.resize(430, 550)
+        self.resize(430, 600)
+        self.context = pipeFile.PathDetails.parse_path()
+        self.project = self.context.project
+        self.entity_type = self.context.entity_type
+        self.work_image_path = self.context.work_image_path
+        self.local_image_path = self.context.local_image_path
+        self.step = self.context.step
+        self.other_dir = self.context.other_dir
+        self.video_path = self.context.work_video_path
+        self.next_version_file = self.context.next_version_file
+        self.not_playblast_step = pipeMira.get_studio_value(self.project, "not_playblast_step")
+        self.has_playblast = self.step not in self.not_playblast_step
+        self.setup_ui()
+
+    def setup_ui(self):
         main_layout = QVBoxLayout(self)
         main_layout.setContentsMargins(0, 0, 0, 0)
 
@@ -150,12 +165,6 @@ class QC(QDialog):
         screen_shot_layout.setStretch(1, 1)
         screen_shot_layout.setStretch(2, 0)
 
-        self.version_check = QCheckBox("%s%s" % (" "*40, "Use this screen shot as a version."))
-
-        valid_file_label = TextLabel("Valid file")
-        self.valid_file_widget = CellQCWidget(valid_file_label)
-        self.valid_file_widget.set_mandatory()
-
         preflight_label = TextLabel("Preflight")
         self.preflight_widget = CellQCWidget(preflight_label)
 
@@ -163,12 +172,25 @@ class QC(QDialog):
         self.playblast_widget = CellQCWidget(playblast_label)
         self.playblast_widget.set_mandatory()
 
+        version_layout = QVBoxLayout()
+        version_layout.setContentsMargins(0, 10, 0, 10)
+        version_label = QLabel("<font color=#ff9c00><b>审核（只限一个文件）</b></font>")
+        self.version_widget = CellQCWidget(DragFileWidget())
+        self.version_widget.set_mandatory()
+        version_layout.addWidget(version_label)
+        version_layout.addWidget(self.version_widget)
+
         qc_label = TextLabel("Step QC")
         self.qc_widget = CellQCWidget(qc_label)
         self.qc_widget.set_mandatory()
 
+        other_layout = QVBoxLayout()
+        other_layout.setContentsMargins(0, 10, 0, 10)
+        other_label = QLabel("<font color=#ff9c00><b>Other</b></font>")
         self.other_widget = CellQCWidget(DragFileWidget())
         self.other_widget.set_mandatory()
+        other_layout.addWidget(other_label)
+        other_layout.addWidget(self.other_widget)
 
         post_qc_label = TextLabel("Post QC")
         self.post_qc_widget = CellQCWidget(post_qc_label)
@@ -177,46 +199,28 @@ class QC(QDialog):
         self.submit_btn = QPushButton("Submit")
 
         main_layout.addLayout(screen_shot_layout)
-        # main_layout.addWidget(self.version_check)
-        main_layout.addWidget(self.valid_file_widget)
         main_layout.addWidget(self.preflight_widget)
-        main_layout.addWidget(self.playblast_widget)
+        if self.has_playblast:
+            main_layout.addWidget(self.playblast_widget)
+        else:
+            main_layout.addLayout(version_layout)
         main_layout.addWidget(self.qc_widget)
-        main_layout.addWidget(self.other_widget)
+        main_layout.addLayout(other_layout)
         main_layout.addWidget(self.post_qc_widget)
         main_layout.addWidget(self.submit_btn)
 
         self.submit_btn.clicked.connect(self.submit)
 
-    def submit(self):
-        # has screen
+    def submit_screen_shot(self):
         self.screen_shot_label.set_status("start")
         thumbnail_path = self.screen_shot_widget.get_thumbnail_path()
         if thumbnail_path:
             self.screen_shot_label.set_status("success")
+            return thumbnail_path
         else:
             self.screen_shot_label.set_status("fail")
-            return
-        # valid file
-        self.valid_file_widget.start()
-        context = pipeFile.PathDetails.parse_path()
-        if context and context.is_local_file() and context.is_working_file():
-            self.valid_file_widget.success()
-        else:
-            self.valid_file_widget.fail()
-            return
-        # if version is 000, save as 001
-        if context.version == "000":
-            next_version_file = context.next_version_file
-            save_as.save_as(next_version_file)
-            context = pipeFile.PathDetails.parse_path()
-        # get path
-        entity_type = context.entity_type
-        work_image_path = context.work_image_path
-        local_image_path = context.local_image_path
-        step = context.step
-        other_dir = context.other_dir
-        # preflight
+
+    def submit_preflight(self):
         if self.preflight_widget.check.isChecked():
             self.preflight_widget.start()
             result, cg = check_gui.main_for_publish()
@@ -224,63 +228,100 @@ class QC(QDialog):
                 cg.close()
                 cg.deleteLater()
                 self.preflight_widget.success()
+                return True
             else:
                 self.preflight_widget.fail()
-                return
-        # playblast
-        not_playblast_step = pipeMira.get_studio_value(context.project, "not_playblast_step")
-        if step in not_playblast_step:
-            self.playblast_widget.check.setChecked(False)
-        if self.playblast_widget.check.isChecked():
+                return False
+        else:
+            return None
+
+    def submit_version(self, thumbnail_path=None):
+        if self.has_playblast:
             self.playblast_widget.start()
             try:
-                if entity_type == "Asset":
+                if self.entity_type == "Asset":
                     playblast_turntable.playblast_turntable()
-                elif entity_type == "Shot":
+                elif self.entity_type == "Shot":
                     playblast_shot.playblast_shot()
                 self.playblast_widget.success()
+                return self.video_path
             except RuntimeError as e:
                 logger.error(str(e))
                 self.playblast_widget.fail()
+        else:
+            version_files = self.version_widget.widget.file_list.all_items_text()
+            if len(version_files) > 1:
+                QMessageBox.warning(None, "Warming Tip", "Only one file can submit once a time.")
+                self.version_widget.fail()
                 return
+            # if version files, copy to video path, match the ext
+            elif len(version_files) == 1:
+                origin_file = version_files[0]
+            else:
+                origin_file = thumbnail_path
+            try:
+                ext = os.path.splitext(origin_file)[-1]
+                version_file = "%s%s" % (os.path.splitext(self.video_path)[0], ext)
+                copy.copy(origin_file, version_file)
+                self.version_widget.success()
+                return version_file
+            except:
+                self.version_widget.fail()
+
+    def submit_step_qc(self):
+        self.qc_widget.start()
+        try:
+            qcpublish(self.step)
+            self.qc_widget.success()
+        except:
+            self.qc_widget.fail()
+
+    def submit_other(self):
+        self.other_widget.start()
+        try:
+            submit_other(self.other_dir, self.other_widget.widget.file_list.all_items_text())
+            self.other_widget.success()
+        except:
+            self.other_widget.fail()
+
+    def submit_post(self, version_file):
+        self.post_qc_widget.start()
+        try:
+            post_qcpublish(self.context, version_file)
+            self.post_qc_widget.success()
+        except:
+            self.post_qc_widget.fail()
+
+    def submit(self):
+        # has screen
+        thumbnail_path = self.submit_screen_shot()
+        if not thumbnail_path:
+            return
+        # preflight
+        preflight_status = self.submit_preflight()
+        if preflight_status is not None:
+            if not preflight_status:
+                return
+        # playblast
+        version_file = self.submit_version(thumbnail_path)
+        if not version_file:
+            return
         # copy image
-        copy.copy(thumbnail_path, local_image_path)
-        copy.copy(thumbnail_path, work_image_path)
+        copy.copy(thumbnail_path, self.local_image_path)
+        copy.copy(thumbnail_path, self.work_image_path)
         os.remove(thumbnail_path)
         # step qc
-        if self.qc_widget.check.isChecked():
-            self.qc_widget.start()
-            try:
-                qcpublish(step)
-                self.qc_widget.success()
-            except:
-                self.qc_widget.fail()
-                return
+        self.submit_step_qc()
         # other
-        if self.other_widget.check.isChecked():
-            self.other_widget.start()
-            try:
-                submit_other(other_dir, self.other_widget.widget.file_list.all_items_text())
-                self.other_widget.success()
-            except:
-                self.other_widget.fail()
-                return
+        self.submit_other()
         # if not preflight, save current file
         if not self.preflight_widget.check.isChecked():
             save_file.save_file()
         # save as next version file
-        next_version_file = context.next_version_file
-        save_as.save_as(next_version_file)
-        logger.info("Save to %s" % next_version_file)
+        save_as.save_as(self.next_version_file)
+        logger.info("Save to %s" % self.next_version_file)
         # post qc
-        if self.post_qc_widget.check.isChecked():
-            self.post_qc_widget.start()
-            try:
-                post_qcpublish(context)
-                self.post_qc_widget.success()
-            except:
-                self.post_qc_widget.fail()
-                return
+        self.submit_post(version_file)
         # close self and pop a message box to tell that all finished
         self.close()
         self.deleteLater()
@@ -288,9 +329,13 @@ class QC(QDialog):
 
 
 def main():
-    from miraLibs.osLibs import get_parent_win
-    qc = QC(get_parent_win.get_parent_win())
-    qc.show()
+    context = pipeFile.PathDetails.parse_path()
+    if context and context.is_local_file() and context.is_working_file():
+        from miraLibs.osLibs import get_parent_win
+        qc = QC(get_parent_win.get_parent_win())
+        qc.show()
+    else:
+        QMessageBox.critical(None, "Warming Tip", "This file is not an valid file.Please check it ^ ^")
 
 
 if __name__ == "__main__":
